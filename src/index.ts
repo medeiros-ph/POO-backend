eb23a70c07481ad3714b4812694629a59042d570
Raphael Medeiros <medeiros.ph@gmail.com>
2023-10-27 00:27:15 +0000

    class & constructor

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,47 @@ let pessoa = {
         console.log(this.nome)
     }
 }
-console.log(pessoa.idade)
+//console.log(pessoa.idade)
+//pessoa.envelhecer()
 
-pessoa.envelhecer()
+// class Gato {
+//     nome: string
+//     idade: number
+//     raca!: string "siames"
+//     constructor(nomeFornecido: string, idadeFornecido: number){
+//         this.nome = nomeFornecido
+//         this.idade = idadeFornecido
+//     }
+//     gatoMia(){
+//         console.log(this.nome)
+//     }
+// }
+
+class Conta {
+    nome: string
+    senha: number
+    id: string
+    private _saldo: number = 0.00
+    constructor(nomeFornecido: string, senhaFornecida: number){
+        this.nome = nomeFornecido
+        this.senha = senhaFornecida
+        this.id = "231132131"
+    }
+
+    deposito (v: number) {
+        this._saldo += v
+    }
+    saque (v: number) {
+        this._saldo -= v
+    }
+    
+    public get saldo() : number {
+        return this._saldo
+    }
+}
+
+const Raphael = new Conta("Raphael Medeiros", 2121)
+
+Raphael.deposito(100000)
+
+console.log(Raphael.saldo)
